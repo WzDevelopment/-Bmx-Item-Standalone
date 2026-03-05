5ebd84c13b55623447b2a703910010f8caf86ad9
LASTV-DEV <lastvroleplay.fivem@gmail.com>
2026-03-05 21:30:05 +0000

    Create items.sql

---
+++ items.sql
@@ -0,0 +1,7 @@
+INSERT INTO `items` (`name`, `label`, `weight`) VALUES ('bmx', 'BMX', 5);
+INSERT INTO `items` (`name`, `label`, `weight`) VALUES ('cruiser', 'Cruiser', 6);
+INSERT INTO `items` (`name`, `label`, `weight`) VALUES ('fixter', 'Fixter', 5);
+INSERT INTO `items` (`name`, `label`, `weight`) VALUES ('scorcher', 'Scorcher VTT', 7);
+INSERT INTO `items` (`name`, `label`, `weight`) VALUES ('tribike', 'TriBike Course', 6);
+INSERT INTO `items` (`name`, `label`, `weight`) VALUES ('tribike2', 'TriBike Sport', 6);
+INSERT INTO `items` (`name`, `label`, `weight`) VALUES ('tribike3', 'TriBike Pro', 6);
