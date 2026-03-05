9f775f9cb665d96bfcc9776d132d918639d95f83
LASTV-DEV <lastvroleplay.fivem@gmail.com>
2026-03-05 21:31:05 +0000

    Create items_qbcore.lua

---
+++ items_qbcore.lua
@@ -0,0 +1,90 @@
+['bmx'] = {
+    ['name'] = 'bmx',
+    ['label'] = 'BMX',
+    ['weight'] = 5000,
+    ['type'] = 'item',
+    ['image'] = 'bmx.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Un vélo BMX pliable'
+},
+
+['cruiser'] = {
+    ['name'] = 'cruiser',
+    ['label'] = 'Cruiser',
+    ['weight'] = 6000,
+    ['type'] = 'item',
+    ['image'] = 'cruiser.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Un vélo de plage'
+},
+
+['fixter'] = {
+    ['name'] = 'fixter',
+    ['label'] = 'Fixter',
+    ['weight'] = 5000,
+    ['type'] = 'item',
+    ['image'] = 'fixter.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Un vélo fixie'
+},
+
+['scorcher'] = {
+    ['name'] = 'scorcher',
+    ['label'] = 'Scorcher VTT',
+    ['weight'] = 7000,
+    ['type'] = 'item',
+    ['image'] = 'scorcher.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Un VTT tout-terrain'
+},
+
+['tribike'] = {
+    ['name'] = 'tribike',
+    ['label'] = 'TriBike Course',
+    ['weight'] = 6000,
+    ['type'] = 'item',
+    ['image'] = 'tribike.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Un vélo de course'
+},
+
+['tribike2'] = {
+    ['name'] = 'tribike2',
+    ['label'] = 'TriBike Sport',
+    ['weight'] = 6000,
+    ['type'] = 'item',
+    ['image'] = 'tribike2.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Un vélo de course sportif'
+},
+
+['tribike3'] = {
+    ['name'] = 'tribike3',
+    ['label'] = 'TriBike Pro',
+    ['weight'] = 6000,
+    ['type'] = 'item',
+    ['image'] = 'tribike3.png',
+    ['unique'] = false,
+    ['useable'] = true,
+    ['shouldClose'] = true,
+    ['combinable'] = nil,
+    ['description'] = 'Un vélo de course professionnel'
+},
